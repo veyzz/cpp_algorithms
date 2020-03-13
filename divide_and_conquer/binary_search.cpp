00619cb4b2f1c0f43f60afb34b7a484e778ca4d4
veyzz <munir.taga@gmail.com>
2020-03-13 16:27:40 +0300

    binary search

---
+++ divide_and_conquer/binary_search.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+int get_pos(std::vector<int> const & arr, int element) {
+    int begin = 0;
+    int end = arr.size();
+    int mid;
+    while (begin <= end) {
+        mid = (begin + end) / 2;
+        if (arr[mid] == element) {
+            return mid + 1;
+        }
+        if (arr[mid] > element) {
+            end = mid - 1;
+        } else {
+            begin = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector<int> arr(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> arr[i];
+    }
+    int k, element;
+    std::cin >> k;
+    for (int i = 0; i < k; ++i) {
+        std::cin >> element;
+        std::cout << get_pos(arr, element) << " ";
+    }
+    std::cout << std::endl;
+	return 0;
+}
